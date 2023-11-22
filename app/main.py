ed950a4259562b396ea1759e051744d3f4175e1f
Spencer Grier <spenceg85@yahoo.com>
2023-11-22 14:27:22 -0500

    Add .env file and retrieve API key from environment

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import APIKeyHeader
 from .routers import search
+import os
 
 app = FastAPI()
 
 api_key_header = APIKeyHeader(name='X-API-KEY')
+
+# Get the API key from the .env file
+api_key = os.environ["OPENAI_API_KEY"]
 
 def get_api_key(api_key: str = Depends(api_key_header)):
     if api_key != "expected_api_key":  # Replace with your actual API key
