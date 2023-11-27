4ef1d21cad98ea5becb1c02af462c8b0d49f86f8
FullSpenceStack <spenceg85@me.com>
2023-11-26 20:59:43 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ api_key_header = APIKeyHeader(name='X-API-KEY')
 api_key = os.environ["API_KEY"]
 
 def get_api_key(api_key: str = Depends(api_key_header)):
-    if api_key != "expected_api_key":  # Replace with your actual API key
+    if api_key != os.environ["API_KEY"]:  # Replace with your actual API key
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid API Key"
