2868de6228f44fa24e006d370c606db88e5b166d
FullSpenceStack <spenceg85@me.com>
2023-11-26 20:36:48 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 api_key_header = APIKeyHeader(name='X-API-KEY')
 
 # Get the API key from the .env file
-api_key = os.environ["OPENAI_API_KEY"]
+api_key = os.environ["API_KEY"]
 
 def get_api_key(api_key: str = Depends(api_key_header)):
     if api_key != "expected_api_key":  # Replace with your actual API key
