a9781842d29ae95e87ba25e6a1e5eff9b443a610
FullSpenceStack <spenceg85@me.com>
2023-11-26 20:44:23 -0500

    Update search.py

--- app/routers/search.py
+++ app/routers/search.py
@@ -7,7 +7,7 @@ router = APIRouter()
 async def search(query: str):
     try:
         # Placeholder implementation, replace with actual Keymate.ai SDK usage
-        response = await httpx.get(f"https://api.keymate.ai/search?query={query}")
+        response = await httpx.get("https://api.keymate.ai/search?query={query}")
         if response.status_code != 200:
             raise HTTPException(status_code=500, detail="Error in Keymate.ai API")
         return response.json()
